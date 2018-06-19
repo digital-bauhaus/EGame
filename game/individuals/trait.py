1763b2853596378fb6a2e27ed3358f127945e39c
André Karge <andre.karge@gmail.com>
2018-06-19 16:09:21 +0200

    relax the check for sum up to 1 for traits from e-10 to e-5

--- game/individuals/trait.py
+++ game/individuals/trait.py
@@ -5,6 +5,9 @@ class Trait:
         """
         check if the dna is valid
         """
-        eps = 1e-10
+        eps = 1e-5
         if abs(1 - sum(dna)) > eps:
-            raise InvalidDNAException("Desire DNA does not sum up to 1.0 !")
+            raise InvalidDNAException(dna, "DNA does not sum up to 1.0 !")
+        for value in dna:
+            if value < 0:
+                raise InvalidDNAException(dna, "DNA is negative!")
